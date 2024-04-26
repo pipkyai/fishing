20bb9c8a68ffd6144caeffab52bf3149825edaad
pipkyai <68229318+pipkyai@users.noreply.github.com>
2024-04-26 18:06:56 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import {useEffect} from "react";
-const tg= window.Telegram.WebApp;
 
 function App() {
 
@@ -8,9 +7,6 @@ function App() {
         tg.ready()
     }, []);
 
-    const onClose = () => {
-        tg.close();
-    }
   return (
     <div className="App">
       hello
